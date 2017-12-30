507fc402ecd6255bb8719e34623e29722822dfbf
KevinK <gkangkai@gmail.com>
2017-12-30 20:27:02 +0800

    643

--- 643_Maximum_Average_Subarray_I.cpp
+++ 643_Maximum_Average_Subarray_I.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
-        int s = 0;
+        int s = 0, r = 0;
         for (int i = 0; i < k && i < nums.size(); i++)
             s += nums[i];
-        double r = s / (double)k;
+        r = s;
         for (int i = 1; i + k <= nums.size(); i++) {
             s = s - nums[i-1] + nums[i+k-1];
-            r = max(r, s / (double)k);
+            r = max(r, s);
         }
-        return r;
+        return r * 1.0 / k;
     }
 };
