184123dd79093f123d399e3708802b4585654d59
KevinK <gkangkai@gmail.com>
2018-01-12 14:13:19 +0800

    160

--- 160_Intersection_of_Two_Linked_Lists.cpp
+++ 160_Intersection_of_Two_Linked_Lists.cpp
@@ -6,13 +6,41 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// class Solution {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode * p1 = headA, * p2 = headB;
+//         while (p1 != p2) {
+//             p1 = p1 == NULL ? headA : p1->next;
+//             p2 = p2 == NULL ? headB : p2->next;
+//         }
+//         return p1;
+//     }
+// };
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode * p1 = headA, * p2 = headB;
-        while (p1 != p2) {
-            p1 = p1 == NULL ? headA : p1->next;
-            p2 = p2 == NULL ? headB : p2->next;
+        int l1 = 0, l2 = 0;
+        while (p1) {
+            p1 = p1->next; l1++;
+        }
+        while (p2) {
+            p2 = p2->next; l2++;
+        }
+        p1 = headA;
+        p2 = headB;
+        while (l1 > l2) {
+            p1 = p1->next; l1--;
+        }
+        while (l1 < l2) {
+            p2 = p2->next; l2--;
+        }
+        while (p1 && p2 && p1 != p2) {
+            p1 = p1->next;
+            p2 = p2->next;
         }
         return p1;
     }
