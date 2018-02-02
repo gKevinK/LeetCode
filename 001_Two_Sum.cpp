80a192f09f46dc10510b01b6b24cf79b889ab497
KevinK <gkangkai@gmail.com>
2018-02-02 23:59:14 +0800

    001

--- 001_Two_Sum.cpp
+++ 001_Two_Sum.cpp
@@ -1,17 +1,32 @@
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         int i, j;
+//         vector<int> r(2);
+//         for (i = 0; i < nums.size() - 1; i++) {
+//             for (j = i + 1; j < nums.size(); j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     r[0] = i;
+//                     r[1] = j;
+//                     return r;
+//                 }
+//             }
+//         }
+//         return r;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int i, j;
-        vector<int> r(2);
-        for (i = 0; i < nums.size() - 1; i++) {
-            for (j = i + 1; j < nums.size(); j++) {
-                if (nums[i] + nums[j] == target) {
-                    r[0] = i;
-                    r[1] = j;
-                    return r;
-                }
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); i++) {
+            if (m.find(nums[i]) == m.end()) {
+                m[target-nums[i]] = i;
+            } else {
+                return vector<int> { m[nums[i]], i };
             }
         }
-        return r;
+        return vector<int>();
     }
 };
