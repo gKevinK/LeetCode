cd45bb72bfcda19bf010d42f907ab7b5208492d9
KevinK <gkangkai@gmail.com>
2018-02-22 14:55:44 +0800

    461

---
+++ 461_Hamming_Distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int s = 0;
+        while (x != y) {
+            s += (x % 2 != y % 2);
+            x >>= 1;
+            y >>= 1;
+        }
+        return s;
+    }
+};
