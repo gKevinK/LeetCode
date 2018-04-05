206813e10b7ab2086bd084b893eb288e94ec11da
KevinK <gkangkai@gmail.com>
2018-04-05 12:00:00 +0800

    275

--- 275_H-Index_II.cpp
+++ 275_H-Index_II.cpp
@@ -9,3 +9,19 @@ public:
         return citations.size();
     }
 };
+
+// Fast, O(log(n))
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int lo = 0, hi = citations.size() - 1, len = citations.size();
+        while (lo <= hi) {
+            int mi = (hi - lo) / 2 + lo;
+            if (len - mi <= citations[mi])
+                hi = mi - 1;
+            else
+                lo = mi + 1;
+        }
+        return len - lo;
+    }
+};
