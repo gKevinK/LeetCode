255b379c57fbcadf7dc64a82fbb0e1db1734e574
KevinK <gkangkai@gmail.com>
2018-06-30 00:30:57 +0800

    628

---
+++ 628_Maximum_Product_of_Three_Numbers.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        int a1 = 0, a2, a3, b1, b2, b3;
+        a1 = a2 = a3 = INT_MIN;
+        b1 = b2 = INT_MAX;
+        for (int & i : nums) {
+            if (a2 >= 0) a3 = max(a3, a2 * i);
+            if (b2 < 0) a3 = max(a3, b2 * i);
+            if (i >= 0) {
+                if (a1 >= 0) a2 = max(a2, a1 * i);
+                a1 = max(a1, i);
+                if (b1 < 0) b2 = min(b2, b1 * i);
+            } else {
+                if (b1 < 0) a2 = max(a2, b1 * i);
+                if (a1 >= 0) b2 = min(b2, a1 * i);
+                b1 = min(b1, i);
+            }
+        }
+        return a3;
+    }
+};
