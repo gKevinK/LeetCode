81d991d313431be4a8f210a2d6ee9c673fcdcdd8
KevinK <gkangkai@gmail.com>
2018-07-25 23:53:44 +0800

    709

---
+++ 709_To_Lower_Case.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string toLowerCase(string str) {
+        for (auto & c : str)
+            if (c >= 'A' && c <= 'Z')
+                c += 'a' - 'A';
+        return str;
+    }
+};
