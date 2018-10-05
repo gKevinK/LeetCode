b5b1cd23df879a642ae264ef95806aaf93b2e416
KevinK <gkangkai@gmail.com>
2018-10-05 23:59:00 +0800

    372

--- 372_Super_Pow.cpp
+++ 372_Super_Pow.cpp
@@ -2,18 +2,11 @@ class Solution {
 public:
     int superPow(int a, vector<int>& b) {
         int x = 1;
-        a = a % 1337;
-        for (int j = 0; j < b[0]; ++j) {
-            x *= a;
-            x = x % 1337;
-        }
-        for (int i = 1; i < b.size(); ++i) {
-            x = x % 1337;
-            int t = x;
-            for (int i = 0; i < 9; ++i) {
-                x = x * t;
-                x = x % 1337;
-            }
+        a %= 1337;
+        for (int i = 0; i < b.size(); ++i) {
+            int t2 = x * x % 1337;
+            int t4 = t2 * t2 % 1337;
+            x = t4 * t4 % 1337 * t2 % 1337;
             for (int j = 0; j < b[i]; ++j) {
                 x *= a;
                 x = x % 1337;
