2ef016848c55b6cc0662fea586af754feffc3a99
KevinK <gkangkai@gmail.com>
2018-10-09 23:36:10 +0800

    376

--- 376_Wiggle_Subsequence.cpp
+++ 376_Wiggle_Subsequence.cpp
@@ -2,30 +2,16 @@ class Solution {
 public:
     int wiggleMaxLength(vector<int>& nums) {
         if (nums.empty()) return 0;
-        bool up;
-        int num = 1, last = nums[0], i;
-        for (i = 1; i < nums.size(); ++i) {
-            if (nums[i] > last) {
-                up = false;
+        int up = 0;
+        int num = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i - 1] && up != 1) {
+                up = 1;
                 num++;
-                last = nums[i];
-                break;
-            } else if (nums[i] < last) {
-                up = true;
-                num++;
-                last = nums[i];
-                break;
-            }
-        }
-        for (++i; i < nums.size(); ++i) {
-            if (nums[i] > last && up == true) {
-                up = false;
-                num++;
-            } else if (nums[i] < last && up == false) {
-                up = true;
+            } else if (nums[i] < nums[i - 1] && up != -1) {
+                up = -1;
                 num++;
             }
-            last = nums[i];
         }
         return num;
     }
