46013cf1f8944c049500be2736ba835737dd75fb
KevinK <gkangkai@gmail.com>
2018-10-19 14:41:23 +0800

    395

---
+++ 395_Longest_Substring_with_At_Least_K_Repeating_Characters.cpp
@@ -0,0 +1,35 @@
+static auto _ = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}();
+
+class Solution {
+public:
+    int longestSubstring(string s, int k) {
+        int res = 0;
+        for (int h = 1; h <= 26; h++) {
+            vector<int> count(26, 0);
+            int i = 0, j = 0, lz = 0, lek = 0;
+            while (j < s.size()) {
+                if (lz <= h) {
+                    int & c = count[s[j] - 'a'];
+                    c++;
+                    if (c == 1) lz++;
+                    if (c == k) lek++;
+                    j++;
+                } else {
+                    int & c = count[s[i] - 'a'];
+                    if (c == k) lek--;
+                    if (c == 1) lz--;
+                    c--;
+                    i++;
+                }
+                if (lz == h && lz == lek) {
+                    res = max(res, j - i);
+                }
+            }
+        }
+        return res;
+    }
+};
