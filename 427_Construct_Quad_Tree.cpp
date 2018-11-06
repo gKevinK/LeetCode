7d110f43399c98189d344d48391ab7cf08e51843
KevinK <gkangkai@gmail.com>
2018-11-06 21:47:44 +0800

    427

--- 427_Construct_Quad_Tree.cpp
+++ 427_Construct_Quad_Tree.cpp
@@ -22,23 +22,24 @@ public:
 };
 */
 class Solution {
+    Node * tl, * fl;
+    
     Node* c(vector<vector<int>>& grid, int x, int y, int len) {
-        if (len == 1) return new Node(grid[x][y], true, nullptr, nullptr, nullptr, nullptr);
+        if (len == 1) return grid[x][y] ? tl : fl;
         int hlen = len / 2;
         vector<Node *> cs(4);
         cs[0] = c(grid, x, y, hlen);
         cs[1] = c(grid, x, y + hlen, hlen);
         cs[2] = c(grid, x + hlen, y, hlen);
         cs[3] = c(grid, x + hlen, y + hlen, hlen);
-        if (cs[0]->isLeaf && cs[1]->isLeaf && cs[2]->isLeaf && cs[3]->isLeaf
-            && (cs[0]->val + cs[1]->val + cs[2]->val + cs[3]->val) % 4 == 0) {
-            for (auto c : cs) delete c;
-            return new Node(grid[x][y], true, nullptr, nullptr, nullptr, nullptr);
-        }
+        if (cs[0] == cs[1] && cs[2] == cs[3] && cs[0] == cs[2])
+            return grid[x][y] ? tl : fl;
         else return new Node(true, false, cs[0], cs[1], cs[2], cs[3]);
     }
 public:
     Node* construct(vector<vector<int>>& grid) {
+        tl = new Node(true, true, nullptr, nullptr, nullptr, nullptr);
+        fl = new Node(false, true, nullptr, nullptr, nullptr, nullptr);
         return c(grid, 0, 0, grid.size());
     }
 };
