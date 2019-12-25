2a2b7c07bf0baa48c315f521c6a3e10f323b596b
KevinK <gkangkai@gmail.com>
2019-12-25 21:22:27 +0800

    855

---
+++ 855_Exam_Room.cpp
@@ -0,0 +1,64 @@
+class ExamRoom {
+    int _N;
+    set<vector<int>> si, sh;
+    
+    int dist(int l, int r) {
+        if (l == 0) return r - 1;
+        if (r == _N) return r - l - 1;
+        return (r - l - 1) / 2;
+    }
+    
+public:
+    ExamRoom(int N) {
+        _N = N;
+        si.insert({ 0, N });
+        sh.insert({ -dist(0, N), 0, N });
+    }
+    
+    int seat() {
+        auto v = *sh.begin();
+        sh.erase(sh.begin());
+        si.erase({ v[1], v[2] });
+        int lo = v[1], hi = v[2], r = (hi - lo - 1) / 2 + lo;
+        if (lo == 0) r = 0;
+        else if (hi == _N) r = _N - 1;
+        if (r - lo) {
+            sh.insert({ -dist(lo, r), lo, r });
+            si.insert({ lo, r });
+        }
+        if (hi - r - 1) {
+            sh.insert({ -dist(r + 1, hi), r + 1, hi });
+            si.insert({ r + 1, hi });
+        }
+        return r;
+    }
+    
+    void leave(int p) {
+        int lo = p, hi = p + 1;
+        auto it = si.upper_bound({ p, 0 });
+        if (it != si.end()) {
+            if ((*it)[0] == p + 1) {
+                hi = (*it)[1];
+                sh.erase({ -dist(p + 1, hi), p + 1, hi });
+                it = si.erase(it);
+            }
+        }
+        if (it != si.begin()) {
+            --it;
+            if ((*it)[1] == p) {
+                lo = (*it)[0];
+                sh.erase({ -dist(lo, p), lo, p });
+                si.erase(it);
+            }
+        }
+        sh.insert({ -dist(lo, hi), lo, hi });
+        si.insert({ lo, hi });
+    }
+};
+
+/**
+ * Your ExamRoom object will be instantiated and called as such:
+ * ExamRoom* obj = new ExamRoom(N);
+ * int param_1 = obj->seat();
+ * obj->leave(p);
+ */
