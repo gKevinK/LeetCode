193eba041625a501b1578cf37fba819f9dbd17d4
KevinK <gkangkai@gmail.com>
2020-08-29 19:57:57 +0800

    898

--- 898_Bitwise_ORs_of_Subarrays.cpp
+++ 898_Bitwise_ORs_of_Subarrays.cpp
@@ -1,17 +1,36 @@
+// class Solution {
+// public:
+//     int subarrayBitwiseORs(vector<int>& A) {
+//         unordered_set<int> res, s1, s2;
+//         for (int & i : A) {
+//             s2 = { i };
+//             for (int j : s1) {
+//                 s2.insert(i | j);
+//             }
+//             for (int j : s2) {
+//                 res.insert(j);
+//             }
+//             s1.swap(s2);
+//         }
+//         return res.size();
+//     }
+// };
+
 class Solution {
 public:
-    int subarrayBitwiseORs(vector<int>& A) {
-        unordered_set<int> res, s1, s2;
-        for (int & i : A) {
-            s2 = { i };
-            for (int j : s1) {
-                s2.insert(i | j);
+    int subarrayBitwiseORs(vector<int> A) {
+        vector<int> res;
+        int left = 0, right;
+        for (int a : A) {
+            right = res.size();
+            res.push_back(a);
+            for (int i = left; i < right; ++i) {
+                if (res.back() != (res[i] | a)) {
+                    res.push_back(res[i] | a);
+                }
             }
-            for (int j : s2) {
-                res.insert(j);
-            }
-            s1.swap(s2);
+            left = right;
         }
-        return res.size();
+        return unordered_set(res.begin(), res.end()).size();
     }
 };
